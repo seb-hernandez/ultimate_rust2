5d59c725fc5fb21493adf6fe3f34d5a0acdb9fa9
Sebastian Hernandez <sebastian.hernandez@digiexam.se>
2024-03-04 12:05:50 +0100

    first commit

---
+++ exercise/testing/tests/more_tests.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod test {
+    use testing::{splish, sploosh};
+
+    #[test]
+    fn test_lib() {
+        assert_eq!(sploosh(splish(-1, 0), splish(1, 1), splish(3, 2)), 4);
+    }
+}
